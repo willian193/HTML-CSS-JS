9aaa46bc6f563863dffef73347ea31545fa47368
willian193 <willianradunzz24@gmail.com>
2026-06-03 07:20:13 -0300

    90

---
+++ Exemplos/arrays/script.js
@@ -0,0 +1,26 @@
+let usuarios = [10 , "carlos" , "Maria"]
+
+console.log(usuarios);
+
+console.log(usuarios[1]);
+
+usuarios.push("Nicolas");
+
+console.log(usuarios);
+
+usuarios.pop();
+
+console.log(usuarios);
+
+usuarios.shift();
+
+console.log(usuarios);
+
+console.log(usuarios.length);
+
+console.log(usuarios.includes("Maria"));
+
+usuarios2 = [10 , "carlos","maria","pedro","Joao"]
+
+let novoArray = usuarios2.slice(0,2)
+console.log(novoArray)
